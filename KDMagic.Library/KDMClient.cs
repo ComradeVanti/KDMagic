3746bea65031ee0dcbce23fca83c40e195d42e5d
KingVanti <kingvanti650@gmail.com>
2019-11-05 14:33:46 +0100

    Setup library structure
    - Added basic structure to the library, without any implementation

---
+++ KDMagic.Library/KDMClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KDMagic.Library
+{
+
+    /// <summary>
+    /// Static client class to interact with KDM files
+    /// </summary>
+    public static class KDMClient
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Gets all KDM files in a given direcory
+        /// </summary>
+        /// <param name="directoryPath">The KDM folder path</param>
+        /// <returns>All KDM files in the given directory</returns>
+        public static KDMFile[] GetFiles(string directoryPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets all invalid KDM files in a given direcory
+        /// </summary>
+        /// <param name="directoryPath">The KDM folder path</param>
+        /// <returns>All invalid KDM files in the given directory</returns>
+        public static KDMFile[] GetInvalidFiles(string directoryPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the given KDM files
+        /// </summary>
+        /// <param name="files">The files that should be deleted</param>
+        public static void DeleteFiles(KDMFile[] files)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
---
+++ KDMagic.Library/KDMFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace KDMagic.Library
+{
+
+    /// <summary>
+    /// Represents a single KDM file and exposes some information
+    /// </summary>
+    public class KDMFile
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// True if the current date falls into the valid time-span
+        /// </summary>
+        public bool Valid { get { return ValidFrom < DateTime.Now && DateTime.Now < ValidTo; } }
+
+        /// <summary>
+        /// The path of this file
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// The name of the movie this kdm validates
+        /// </summary>
+        public string MovieName { get; private set; }
+
+        /// <summary>
+        /// The start time of the valid time-span
+        /// </summary>
+        public DateTime ValidFrom { get; private set; }
+
+        /// <summary>
+        /// The end time of the valid time-span
+        /// </summary>
+        public DateTime ValidTo { get; private set; }
+
+        #endregion
+
+    }
+}
