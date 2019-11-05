c9f525725fa054ebf8e9a4fdb1fa8b86e681cefb
KingVanti <kingvanti650@gmail.com>
2019-11-05 14:42:39 +0100

    Added Caliburn.Micro and material design

---
+++ KDMagic.WPF/Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+using KDMagic.WPF.ViewModels;
+using System.Windows;
+
+namespace KDMagic.WPF
+{
+    /// <summary>
+    /// Bootstrapper class that initialized the application
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Initialized the bootstrapper
+        /// </summary>
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets the default window as an <see cref="ClientViewModel"/> 
+        /// </summary>
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ClientViewModel>();
+        }
+
+        #endregion
+
+    }
+}
---
+++ KDMagic.WPF/ViewModels/ClientViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KDMagic.WPF.ViewModels
+{
+    public class ClientViewModel
+    {
+    }
+}
