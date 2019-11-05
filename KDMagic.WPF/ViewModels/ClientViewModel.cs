86815632df654bb62badff5cf6a3a2efda5bd038
KingVanti <kingvanti650@gmail.com>
2019-11-05 15:03:24 +0100

    Wired up Clientview
    Bound properties in view to viewmodel

--- KDMagic.WPF/ViewModels/ClientViewModel.cs
+++ KDMagic.WPF/ViewModels/ClientViewModel.cs
@@ -1,10 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Caliburn.Micro;
+using KDMagic.Library;
+using System;
+using System.IO;
 
 namespace KDMagic.WPF.ViewModels
 {
-    public class ClientViewModel
+
+    /// <summary>
+    /// Viewmodel for the main client view
+    /// </summary>
+    public class ClientViewModel : Screen
     {
+
+        #region Fields
+
+        private string directoryPath;
+        private KDMFile[] invalidFiles = new KDMFile[0];
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// True if the user has selected a valid directory
+        /// </summary>
+        public bool CanScan
+        {
+            get
+            {
+                return Directory.Exists(DirectoryPath);
+            }
+        }
+
+        /// <summary>
+        /// True if there is at least one invalid KDM file
+        /// </summary>
+        public bool CanDelete
+        {
+            get
+            {
+                return InvalidCount > 0;
+            }
+        }
+
+        /// <summary>
+        /// The number of invalid KDM files
+        /// </summary>
+        public int InvalidCount
+        {
+            get
+            {
+                return invalidFiles.Length;
+            }
+        }
+
+        /// <summary>
+        /// The current selected KDM folder path
+        /// </summary>
+        public string DirectoryPath
+        {
+            get { return directoryPath; }
+            set
+            {
+                directoryPath = value;
+                NotifyOfPropertyChange(() => DirectoryPath);
+                NotifyOfPropertyChange(() => CanScan);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Allows the user to browse the explorer for a browser path
+        /// </summary>
+        public void Browse()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Scans the folder for KDM files
+        /// </summary>
+        public void Scan()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the selected KDM files
+        /// </summary>
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
     }
 }
