31d905fbaf36e3dc1e96194bc44f05c0d7594874
KingVanti <kingvanti650@gmail.com>
2021-10-29 09:43:49 +0200

    Add test for file-not found

---
+++ KDMagic.Core.Tests/IngestFileTests.fs
@@ -0,0 +1,16 @@
+﻿module KDMagic.Core.Tests.IngestFileTests
+
+open KDMagic
+open Xunit
+open IngestFile
+
+[<Fact>]
+let ``Ingesting a non-existent file returns a specific error`` () =
+    async {
+        let! result = tryIngestFromPath ""
+
+        match result with
+        | Error (FileNotFound _) -> true
+        | _ -> false
+        |> Assert.True
+    }
