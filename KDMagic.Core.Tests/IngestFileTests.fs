591c1eb87731325b50f673f4161ac8a5356d2b34
KingVanti <kingvanti650@gmail.com>
2021-10-29 09:52:47 +0200

    Refactor test to property

--- KDMagic.Core.Tests/IngestFileTests.fs
+++ KDMagic.Core.Tests/IngestFileTests.fs
@@ -1,16 +1,18 @@
 ﻿module KDMagic.Core.Tests.IngestFileTests
 
+open System.IO
+open FsCheck.Xunit
 open KDMagic
-open Xunit
+open global.Xunit
+open FsCheck
 open IngestFile
 
-[<Fact>]
-let ``Ingesting a non-existent file returns a specific error`` () =
-    async {
-        let! result = tryIngestFromPath ""
+[<Property>]
+let ``Ingesting a non-existent file returns a file-not-found error`` path =
+    path |> (not << File.Exists)
+    ==> lazy
+        (let result = tryIngestFromPath "" |> Async.RunSynchronously
 
-        match result with
-        | Error (FileNotFound _) -> true
-        | _ -> false
-        |> Assert.True
-    }
+         match result with
+         | Error (FileNotFound _) -> true
+         | _ -> false)
