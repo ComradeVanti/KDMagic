27af2841506b215a2aa7c8f9d36dfb8055607caf
KingVanti <kingvanti650@gmail.com>
2021-10-29 10:22:48 +0200

    Add logic for generating test file paths

---
+++ KDMagic.Core.Tests/TestFileGen.fs
@@ -0,0 +1,21 @@
+﻿module KDMagic.Core.Tests.TestFileGen
+
+open System
+open System.IO
+open FsCheck
+
+let private testFileFolderPath =
+    Path.Combine(AppContext.BaseDirectory, "TestFiles")
+
+let private testFilePaths =
+    if Directory.Exists testFileFolderPath then
+        Directory.GetFiles testFileFolderPath |> Array.toList
+    else
+        failwith "No test-files found!"
+
+let genFilePath = Gen.elements testFilePaths
+
+type TestFilePath = TestFilePath of string
+
+type ArbFilePaths =
+    static member Test = genFilePath |> Gen.map TestFilePath |> Arb.fromGen
