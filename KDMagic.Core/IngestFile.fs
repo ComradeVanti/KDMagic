b4d27c9f2845c48a0aeb72f6bdb0eb4090ca8d2f
KingVanti <kingvanti650@gmail.com>
2021-10-29 09:13:51 +0200

    Add logic for reading files

---
+++ KDMagic.Core/IngestFile.fs
@@ -0,0 +1,20 @@
+﻿module KDMagic.IngestFile
+
+open System.IO
+
+type Path = string
+
+type IngestError = FileNotFound of Path
+
+type IngestResult = Result<KDM, IngestError>
+
+let tryIngest file = ()
+
+let tryIngestFromPath path =
+    async {
+        if File.Exists path then
+            let! file = File.ReadAllTextAsync path |> Async.AwaitTask
+            return Ok(tryIngest file)
+        else
+            return Error(FileNotFound path)
+    }
