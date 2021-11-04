0398838ebc398c3f10e3ae731249064f23db6031
KingVanti <kingvanti650@gmail.com>
2021-11-04 19:26:33 +0100

    Add logic for generating digital cinema-names

---
+++ KDMagic.Core.Tests/Gen/DigitalCinemaNameGen.fs
@@ -0,0 +1,17 @@
+﻿module KDMagic.DigitalCinemaNameGen
+
+open FsCheck
+open KDMagic.FilmTitleGen
+open KDMagic.LanguageGen
+open KDMagic.VersionNumberGen
+
+Arb.register<ArbFilmTitles> |> ignore
+Arb.register<ArbVersionNumbers> |> ignore
+Arb.register<ArbLanguages> |> ignore
+
+let genDigitalCinemaName = Arb.generate<DigitalCinemaName>
+
+type ValidDigitalCinemaName = ValidDigitalCinemaName of DigitalCinemaName
+
+type ArbDigitalCinemaNames =
+    static member Valid = genDigitalCinemaName |> asArbOf ValidDigitalCinemaName
