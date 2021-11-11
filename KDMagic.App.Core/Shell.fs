b08a17b4e6a69f5528fe454c369479268ebdbfae
KingVanti <kingvanti650@gmail.com>
2021-11-11 08:46:48 +0100

    Add shell component

---
+++ KDMagic.App.Core/Shell.fs
@@ -0,0 +1,14 @@
+﻿[<RequireQualifiedAccess>]
+module KDMagic.App.Shell
+
+open Elmish
+
+type State = unit
+
+[<RequireQualifiedAccess>]
+type Msg = unit
+
+
+let initial = (), Cmd.none
+
+let update msg state = state, Cmd.none
