78370b3c1daff1bd59009d5568cf4c75db2ac7dd
KingVanti <kingvanti650@gmail.com>
2021-11-13 21:27:31 +0100

    Fix json parsing bug

--- KDMagic.App.Core/Json.fs
+++ KDMagic.App.Core/Json.fs
@@ -9,13 +9,13 @@ let tryParse<'a> text =
 
     let mutable hadError = false
 
-    let errorHandler = EventHandler<ErrorEventArgs>(fun _ _ -> hadError <- true)
+    let errorHandler =
+        EventHandler<ErrorEventArgs>(fun _ error -> hadError <- true)
 
     let settings = JsonSerializerSettings(Error = errorHandler)
 
     let parsed = JsonConvert.DeserializeObject<'a>(text, settings)
 
-    if hadError then Some parsed else None
+    if hadError then None else Some parsed
 
-let stringify item =
-    JsonConvert.SerializeObject item
+let stringify item = JsonConvert.SerializeObject item
